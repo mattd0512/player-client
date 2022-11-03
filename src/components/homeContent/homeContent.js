fc6146541da2b294014c2211e85bfb975cb10669
Abdisheik3 <abdisheikh765@gmail.com>
2022-11-03 16:32:58 -0500

    updating home page

---
+++ src/components/homeContent/homeContent.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Form from 'react-bootstrap/Form'
+
+const HomeContent = () => {
+    return (
+    <>
+   <div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+  <div class="row align-items-center">
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+  <div class="row align-items-end">
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+</div>
+    
+    </>
+    )
+}
+
+
+
+export default HomeContent
